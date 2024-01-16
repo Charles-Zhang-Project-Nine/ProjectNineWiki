06ccc7dce97364e68466e0aeee4e10bd2579d515
Charles Zhang <szinubuntu@gmail.com>
2024-01-16 13:50:36 -0500

    [Script] Update ToC generator
    - Fix topic README
    - Enhance auto-subtopic parsing

--- _Scripts/Pure/GenerateTableOfContents.cs
+++ _Scripts/Pure/GenerateTableOfContents.cs
@@ -1,65 +1,83 @@
 /* Generate Table of Contents
-Version: v0.1
+Version: v0.2
 Author: Charles Zhang
 Initial Date: 2024-01-16
 
 This script generates plain text table of contents to STD OUT to serve as automatic replacement for top-level README for language specific landing page. The contents are saved to git explicitly providing benefit of explicit definitions.
 
 Remark: Current workflow generates most pages, then for WuGong we will manually generate a seperate output, then manually combine them all.
+
+Changelog:
+* v0.1: Basic folder enumeration
+* v0.2: Fix topic README display; Automatically flatten sub-topics to top level
 */
 
 using System.Text;
 using System.Text.RegularExpressions;
 
-// Main function
+// Main parsing logic
+void EnumerateTopic(StringBuilder builder, string topicFolder, string parentPath = "" /*Includes trailing `/`*/)
+{
+    string topic = Path.GetFileName(topicFolder);
+    WriteLine($"Parsing {topic}...");
+    
+    // Skipping icon for now since we don't have it anyway
+    builder.AppendLine($"### {topic}\n");
+
+    // Get topic description
+    string topicREADMEFile = Path.Combine(topicFolder, "README.md");
+    if (File.Exists(topicREADMEFile))
+    {
+        string topicREADME = File.ReadAllText(topicREADMEFile).TrimEnd();
+        string refinedFormat = Regex.Replace(topicREADME, "^# (.*)$", string.Empty, RegexOptions.Multiline).Trim(); // Replace the top-level header
+        refinedFormat = Regex.Replace(refinedFormat, "^#", "####", RegexOptions.Multiline); // Further indent sections
+        builder.AppendLine($"{refinedFormat}\n");
+    }
+
+    // Skip table generation for empty topics
+    if (Directory.EnumerateFiles(topicFolder)
+        .Where(f => Path.GetFileName(f) != "README.md").Count() == 0
+        && Directory.EnumerateDirectories(topicFolder).Count() == 0)
+    {
+        builder.AppendLine($"No contents available under this topic.\n");
+        return;
+    }
+
+    // Generate table of entries
+    builder.AppendLine("|Entry|Description|Link|");
+    builder.AppendLine("|-|-|-|");
+    foreach(string file in Directory.EnumerateFiles(topicFolder)
+        .Where(f => Path.GetFileName(f) != "README.md")
+        .OrderBy(f => f))
+    {   
+        string entryName = Path.GetFileNameWithoutExtension(file);
+        WriteLine($"  Parsing {entryName}");
+
+        string[] contents = File.ReadAllLines(file);
+        string description = contents
+            .Where(l => !string.IsNullOrWhiteSpace(l))
+            .Where(l => !l.StartsWith('#')) // Skip headers
+            .Where(l => !l.StartsWith('>')) // Skip quotes
+            .FirstOrDefault(l => !Regex.IsMatch(l, @"^(.*?): (.*)[^\.]$"));    // First non-meta-data
+            // Remark: Current parsing scheme can encounter problems when the description paragraph contains ':', otherwise we require ending the line with a `.`
+        
+        builder.AppendLine($"|{entryName}|{description ?? "Pending"}|[Link](./{parentPath}{topic}/{entryName.Replace(" ", "%20")})|");  // Relative to language landing page
+    }
+    builder.AppendLine();
+
+    // Recursively parse subtopics (bring them to top level)
+    foreach(string subtopicsFolder in Directory.EnumerateDirectories(topicFolder)
+        .OrderBy(f => f))
+        EnumerateTopic(builder, subtopicsFolder, $"{parentPath}{topic}/");
+}
+
+// Top-level function
 string GenerateToC(string folder)
 {
     StringBuilder builder = new();
     foreach (string topicFolder in Directory.EnumerateDirectories(folder)
         .OrderBy(f => f))
-    {
-        string topic = Path.GetFileName(topicFolder);
-        WriteLine($"Parsing {topic}...");
-        
-        // Skipping icon for now since we don't have it anyway
-        builder.AppendLine($"### {topic}\n");
-
-        string topicREADMEFile = Path.Combine(topicFolder, "README.md");
-        if (File.Exists(topicREADMEFile))
-        {
-            string topicREADME = File.ReadAllText(topicREADMEFile).TrimEnd();
-            string refinedFormat = Regex.Replace(topicREADME, "^#", "####", RegexOptions.Multiline);
-            builder.AppendLine($"{refinedFormat}\n");
-        }
-        if (Directory.EnumerateFiles(topicFolder).Where(f => Path.GetFileName(f) != "README.md").Count() == 0)
-        {
-            builder.AppendLine($"No contents available under this topic.\n");
-            continue;
-        }
-
-        builder.AppendLine("|Entry|Description|Link|");
-        builder.AppendLine("|-|-|-|");
-
-        foreach(string file in Directory.EnumerateFiles(topicFolder)
-            .Where(f => Path.GetFileName(f) != "README.md")
-            .OrderBy(f => f))
-        {   
-            string entryName = Path.GetFileNameWithoutExtension(file);
-            WriteLine($"  Parsing {entryName}");
-
-            string[] contents = File.ReadAllLines(file);
-            string description = contents
-                .Where(l => !string.IsNullOrWhiteSpace(l))
-                .Where(l => !l.StartsWith('#')) // Skip headers
-                .Where(l => !l.StartsWith('>')) // Skip quotes
-                .FirstOrDefault(l => !Regex.IsMatch(l, @"^(.*?): (.*)[^\.]$"));    // First non-meta-data
-                // Remark: Current parsing scheme can encounter problems when the description paragraph contains ':', otherwise we require ending the line with a `.`
-            
-            builder.AppendLine($"|{entryName}|{description ?? "Pending"}|[Link](./{topic}/{entryName.Replace(" ", "%20")})|");  // Relative to language landing page
-        }
-
-        builder.AppendLine();
-    }
+        EnumerateTopic(builder, topicFolder);
     return builder.ToString().TrimEnd();
 }
 
